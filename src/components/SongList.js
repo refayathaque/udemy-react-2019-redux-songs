765863727bf5055e621516f6d096922096dd0ab5
Refayat Haque <refayathaque@gmail.com>
2019-04-15 17:31:41 -0400

    fasdfasdfsf

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,14 +1,41 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+// ^ Function (also a React component) that lets us connect to the Redux Store in the Provider tag/component
 
 class SongList extends Component {
+
+  renderList() {
+    return this.props.songs.map((song) => {
+      return (
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <button className="ui button primary">
+              Select
+            </button>
+          </div>
+          <div className="content">
+            {song.title}
+          </div>
+        </div>
+      );
+    });
+  };
+
   render() {
+    console.log(this.props)
     return (
-      <div>
-        SongList
+      <div className="ui divided list">
+        {this.renderList()}
       </div>
     );
   };
 };
 
-export default connect()(SongList);
+const mapStateToProps = (state) => {
+  console.log(state)
+  return {
+    songs: state.songs
+  };
+}
+
+export default connect(mapStateToProps)(SongList);
